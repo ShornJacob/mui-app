ce6b83bd08f583b74d4b18a617e81e8faf9ac75d
Shorn Jacob <shorn@transparency.consulting>
2019-11-01 15:28:19 +1300

    A container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  }
+}))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const classes = useStyles()
+
+  return <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <div className={classes.paper}></div>
+  </Container>;
 }
 
 export default App;
+
+
+//https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/templates/sign-in/SignIn.js
+
+//https://material-ui.com/api/container/
+//component	elementType	The component used for the root node. Either a string to use a DOM element or a component.
